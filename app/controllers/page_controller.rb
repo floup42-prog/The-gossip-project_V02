e5211375e58a775b316d924be9f5425937f3f5b4
floup42 <pousseurdec@gmail.com>
2021-02-08 21:57:26 +0100

    PLS

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,5 @@
+class PageController < ApplicationController
+  def home
+    @user = first_name
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'page/home'
+  # get 'welcome/show'
   get 'fix_pages/contact'
   get 'static_pages/team'
+  get 'welcome/:user_entry', to: 'welcome#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
